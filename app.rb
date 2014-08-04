2a40f8c77d340c1c18e01ddebd78f34e21ad07c4
Katrina Owen <katrina.owen@gmail.com>
2014-08-03 22:05:14 -0700

    Expose exceptions from analyzer explicitly
    Later we may want to hide these, but for now this should make
    it easier to debug the issues.

--- app.rb
+++ app.rb
@@ -37,12 +37,16 @@ module Analysseur
         halt 400, {error: "cannot analyze code if it's not there"}.to_json
       end
 
-      analysis = Exercism::Analysis.new(config.adapter.new(code)).run(*config.analyzers)
-      results = analysis.values.reject {|result|
-        result.feedback.empty?
-      }.map {|result|
-        {type: result.type, keys: result.feedback.map(&:type).uniq}
-      }
+      begin
+        analysis = Exercism::Analysis.new(config.adapter.new(code)).run(*config.analyzers)
+        results = analysis.values.reject {|result|
+          result.feedback.empty?
+        }.map {|result|
+          {type: result.type, keys: result.feedback.map(&:type).uniq}
+        }
+      rescue => e
+        halt 500, {error: e.message + "\n" + e.backtrace[0] }.to_json
+      end
       halt 200, {results: results}.to_json
     end
   end
