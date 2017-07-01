c6b64f3688db8742ebd38521eedd8d776fe2af79
budmc29 <chirica.mugurel@gmail.com>
2017-07-01 01:23:47 +0100

    Don't run shebang analyzer on test files

--- app.rb
+++ app.rb
@@ -42,15 +42,31 @@ module RikkiRubyAnalyzer
 
       begin
         analysis = Exercism::Analysis.new(config.adapter.new(code)).run(*config.analyzers)
-        results = analysis.values.reject {|result|
-          result.feedback.empty?
-        }.map {|result|
-          {type: result.type, keys: result.feedback.map(&:type).uniq}
-        }
+
+        results = reject_invalid_results(code, analysis.values)
+
+        results = results.map do |result|
+          { type: result.type, keys: result.feedback.map(&:type).uniq }
+        end
       rescue => e
         halt 500, {error: e.message + "\n" + e.backtrace[0] }.to_json
       end
       halt 200, {results: results}.to_json
+    end
+
+    def code_from_test_file?(code)
+      test_file_regex = /class\s*.*Test\s*<\s*Minitest::Test\s*/
+      return true if code.match(test_file_regex)
+    end
+
+    def reject_invalid_results(code, analysis_values)
+      results = analysis_values.reject{ |r| r.feedback.empty? }
+
+      if code_from_test_file?(code)
+        results = results.reject{ |r| r[:type] == :shebang }
+      end
+
+      results
     end
   end
 end
--- test/app_test.rb
+++ test/app_test.rb
@@ -26,4 +26,33 @@ class AppTest < Minitest::Test
     expected = "{\"results\":[{\"type\":\"shebang\",\"keys\":[\"shebang\"]}]}"
     assert_equal expected, last_response.body
   end
+
+  def test_ignores_shebang_for_test_files
+    code = <<~CODE
+      #!/usr/bin/env ruby
+      require 'minitest/autorun'
+      require_relative 'exercise'
+
+      # Common test data version: 1.0.0 e9e9ee9
+      class    ExerciseTest    <    Minitest::Test
+      end
+    CODE
+    post "/analyze/ruby", {code: code}.to_json
+
+    expected = "{\"results\":[]}"
+    assert_equal expected, last_response.body
+  end
+
+  def test_does_not_trigger_false_positive_for_regular_files
+    code = <<~CODE
+      #!/usr/bin/env ruby
+
+      class FooTest < FooTestParent
+      end
+    CODE
+    post "/analyze/ruby", {code: code}.to_json
+
+    expected = "{\"results\":[{\"type\":\"shebang\",\"keys\":[\"shebang\"]}]}"
+    assert_equal expected, last_response.body
+  end
 end
